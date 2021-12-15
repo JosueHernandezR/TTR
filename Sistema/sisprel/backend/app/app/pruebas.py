8d9191186e70aff02ee3fe96ac070ed8894c00da
Josué David Hernández Ramírez <justinhernandez054@gmail.com>
2021-12-15 00:11:18 -0600

    Preparando proyeto para presentacion final

--- Sistema/sisprel/backend/app/app/pruebas.py
+++ Sistema/sisprel/backend/app/app/pruebas.py
@@ -1,194 +1,114 @@
-
-from typing import Any, List
-import numpy as np
-import math
-
-def factorial(numero):
-    if numero <= 0:
-        return 1
-    factorial = 1
-    while numero > 0:
-        factorial = factorial * numero
-        numero -= 1
-    return factorial
-
-def evalua_ecuacion_general(r:int,l:int, pn: float):
-    fact: float = 0
-    fact += ((factorial(r))/(factorial(l)*factorial(r-l)))*((math.pow(pn,l)))*(math.pow((1-pn),(r-l)))
-    return fact
-
-def evalua_ecuacion_general_derivada(r:int, l: int,pcr: float):
-    fact = (factorial(r)/(factorial(l)*factorial(r-l)))*((-1)*math.pow(pcr,l)*(r-l)*(math.pow((1-pcr),(r-l-1)))+(l)*(math.pow((1-pcr),(r-l)))*(math.pow(pcr,(l-1))))
-    return fact
-
-def set_lamdar(pcr: float, r:int):
-    lambdar: float = 0
-    i: int = 0
-    m: int
-    l: int
-
-    if((r%2)==0):
-        m = (r/2) + 1
-    else:
-        m = (r+1) / 2
-    l = m
-
-    for l in np.arange(l,r,1):
-        lambdar += evalua_ecuacion_general_derivada(r,l,pcr)
-    
-    return lambdar
-
-def set_resultados_ecuacion_general(n: int, pn: float, r:int):  #Ecuacion 6 Grupo de votacion de tamaño r
-    pr: float = 0.0
-    aux: float
-    m: int
-    l: int
-    i: int = 0
-    lista_resultados: list = list()
-
-    if (r%2)==0:
-        m=(r/2)+1
-    else:
-        m=(r+1)/2
-    l=m
-    # Calcula eliminacion total
-    if n==-1:
-        n = 0
-        while pn>0.01:
-            try:
-                for l in np.arange(l, r, 1):
-                    pr += evalua_ecuacion_general(r,l,pn)
-
-                lista_resultados.append(pr)
-                n += 1
-                if pn == pr:
-                    break
-                else:
-                    pn=pr
-                    pr = 0
-                    i +=1
-            except:
-                print("Fallo")
-
-        return lista_resultados
-
-    else:
-        for i in np.arange(0,n,1):
-            try:
-                for l in np.arange(l,r,1):
-                    pr+=evalua_ecuacion_general(r,l,pn)
-                lista_resultados.append(pr)
-                pn=pr
-                pr=0
-            except:
-                print("Fallo")
-        return lista_resultados
-
-def set_pcr(r:int):
-    i: float = 0.0
-    iaux: int = 0
-    m: int
-    l: int
-    primera=1
-    pr = 0.0
-    praux=0.0
-    resta=-1.0
-    resta2=0.0
-    pcr: float
-
-    if r==2:
-        pcr=1.0
-        return pcr
-    else:
-        if (r%2)==0: #Par
-            for iaux in np.arange(iaux,100,1):
-                i = iaux/100.0
-                m = (r/2)+1
-                l = m
-                for l in np.arange(l,r,1):
-                    pr += evalua_ecuacion_general(r,l,i)
-                for l in np.arange(l,r,1):
-                    praux += evalua_ecuacion_general(r,m,pr)
-
-                if (((pr-praux) > 0) or ((pr-praux) < 0) and primera == 1):
-                    primera = 0
-                    resta = pr-praux
-                else:
-                    if (((pr-praux) > 0) or ((pr-praux) < 0) and (primera == 0)):
-                        resta2 = pr-praux
-                        if (((resta > 0) and (resta2 < 0)) or ((resta < 0) and (resta2 > 0))):
-                            pcr = i
-                            return pcr
-            
-        else:
-            if ((r%2) != 0): #Par
-                pcr = 0.50
-                return pcr
-
-def set_pndr(lambdar: float, pcr: float, n: int):
-    #Para n subniveles
-    pndr = (pcr)*(1-(math.pow(lambdar,(-1*n))))
-    # Devolviendolo en porcentaje
-    return pndr
-
-#Umbral superior de credibilidad
-def set_pnfr(pndr: float, lamdar: float, n: int):
-    #Para n subniveles
-    pnrf = pndr + math.pow(lamdar,(-1*n))
-    # Devolviendolo en porcentaje
-    return pnrf
-def set_pn(n:int, p0:float, r:int):
-    n_resultados:int
-    resultados: list
-    pn: float
-    i: int = 0
-    resultados = set_resultados_ecuacion_general(n,p0,r)
-    n_resultados = n
-    pn=resultados[(n_resultados-1)]
-    for i in np.arange(i, n_resultados, 1):
-        resultados[i] *=100
-    pn *=100
-    #Aqui hay error
-    print(resultados)
-    return pn
-
-p0: float = 69
+participacion_total_azc = 223965
+participacion_total_coy = 383324
+participacion_total_gam = 602727
 
 
+pan_azc = 43050
+pan_coy = 74610
+pan_gam = 59273
 
-i: int =0
-r:int=4
-n = 3
-pcr = set_pcr(r)
+pri_azc = 18990
+pri_coy = 35793
+pri_gam = 54572
 
-if((r%2)==0):
-    m = (r/2) + 1
-else:
-    m = (r+1) / 2
+prd_azc = 26152
+prd_coy = 85869
+prd_gam = 100749
 
-print("PCR", pcr)
-# pr: float = 0.0
-if (r%2)==0:
-    m=(r/2)+1
-else:
-    m=(r+1)/2
-l=m
-pn_test = set_pn(n,p0,r)
-print("pn", pn_test)
-lambdar = set_lamdar(pcr,r)
-pndr = set_pndr(lambdar,pcr,n)
-pnfdr = set_pnfr(pndr,lambdar,n)
-ecuacion_general_derivada = evalua_ecuacion_general_derivada(r,l,pcr)
-ecuacion_general = evalua_ecuacion_general(r,l,pn_test)
-eliminacion_total = set_resultados_ecuacion_general(-1,p0,r)
+verde_azc = 7408
+verde_coy = 11846
+verde_gam = 26324
+
+pt_azc = 5605
+pt_coy = 9353
+pt_gam = 13620
+
+mc_azc = 5210
+mc_coy = 9353
+mc_gam = 13163
+
+morena_azc = 100803
+morena_coy = 116403
+morena_gam = 254239
+
+es_azc = 4351
+es_coy = 4441
+es_gam = 11229
+
+ph_azc = 4569
+ph_coy = 8186
+ph_gam = 16346
+
+pan_azc_por = (pan_azc/participacion_total_azc)*100
+pan_coy_por = (pan_coy/participacion_total_coy)*100
+pan_gam_por = (pan_gam/participacion_total_gam)*100
+
+pri_azc_por = (pri_azc/participacion_total_azc)*100
+pri_coy_por = (pri_coy/participacion_total_coy)*100
+pri_gam_por = (pri_gam/participacion_total_gam)*100
+
+prd_azc_por = (prd_azc/participacion_total_azc)*100
+prd_coy_por = (prd_coy/participacion_total_coy)*100
+prd_gam_por = (prd_gam/participacion_total_gam)*100
+
+verde_azc_por = (verde_azc/participacion_total_azc)*100
+verde_coy_por = (verde_coy/participacion_total_coy)*100
+verde_gam_por = (verde_gam/participacion_total_gam)*100
+
+pt_azc_por = (pt_azc/participacion_total_azc)*100
+pt_coy_por = (pt_coy/participacion_total_coy)*100
+pt_gam_por = (pt_gam/participacion_total_gam)*100
+
+mc_azc_por = (mc_azc/participacion_total_azc)*100
+mc_coy_por = (mc_coy/participacion_total_coy)*100
+mc_gam_por = (mc_gam/participacion_total_gam)*100
+
+morena_azc_por =(morena_azc/participacion_total_azc)*100
+morena_coy_por =(morena_coy/participacion_total_coy)*100
+morena_gam_por =(morena_gam/participacion_total_gam)*100
 
 
-print("Lambdar",lambdar)
-print("PNDR",pndr)
-print("PNFDR",pnfdr)
-print("Ecuacion general derivada", ecuacion_general)
+es_azc_por = (es_azc/participacion_total_azc)*100
+es_coy_por = (es_coy/participacion_total_coy)*100
+es_gam_por = (es_gam/participacion_total_gam)*100
 
-resultados_ecuacion_general = set_resultados_ecuacion_general(n,pn_test,r)
-print("Resultados ecuacion general", resultados_ecuacion_general)
-print("PN test",pn_test)
-print("eliminacion total", eliminacion_total)
+ph_azc_por = (ph_azc/participacion_total_azc)*100
+ph_coy_por = (ph_coy/participacion_total_coy)*100
+ph_gam_por = (ph_gam/participacion_total_gam)*100
+
+print("PAN")
+print(pan_azc_por)
+print(pan_coy_por)
+print(pan_gam_por)
+print("PRI")
+print(pri_azc_por)
+print(pri_coy_por)
+print(pri_gam_por)
+print("prd")
+print(prd_azc_por)
+print(prd_coy_por)
+print(prd_gam_por)
+print("verde")
+print(verde_azc_por)
+print(verde_coy_por)
+print(verde_gam_por)
+print("pt")
+print(pt_azc_por)
+print(pt_coy_por)
+print(pt_gam_por)
+print("mc")
+print(mc_azc_por)
+print(mc_coy_por)
+print(mc_gam_por)
+print("morena")
+print(morena_azc_por)
+print(morena_coy_por)
+print(morena_gam_por)
+print("es")
+print(es_azc_por)
+print(es_coy_por)
+print(es_gam_por)
+print("ph")
+print(ph_azc_por)
+print(ph_coy_por)
+print(ph_gam_por)
